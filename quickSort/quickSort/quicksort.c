1c738dcda782195322f32dad40802800e96a762a
MinyazevR <eeuriset8@gmail.com>
2021-09-24 03:23:01 +0500

    adding tests

--- quickSort/quickSort/quicksort.c
+++ quickSort/quickSort/quicksort.c
@@ -74,12 +74,67 @@ void smartQSort(int* arrayOfNumber, int initialElement, int endElement)
     }
 }
 
+// Функция для проверки отсортированности массива
+bool sortingСheck(int* arrayOfNumber, int numberOfElements)
+{
+    for (int i = 0; i < numberOfElements - 1; i++)
+    {
+        if (arrayOfNumber[i] > arrayOfNumber[i + 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Функция для тестирования сортировки QSort
+testCorrectQSort()
+{
+    // Массив из случайных чисел
+    int arrayOfNumber[10] = { 9, 7, 8, 6, 3, 1, 7, 0, 3, 2 };
+    smartQSort(arrayOfNumber, 0, 9);
+
+    // Массив из равных чисел
+    int arrayOfEqualValues[100] = { 0 };
+    for (int i = 0; i < 100; i++)
+    {
+        arrayOfEqualValues[i] = 12;
+    }
+    smartQSort(arrayOfEqualValues, 0, 99);
+
+    // Отсортированный массив
+    int sortedArray[100] = { 0 };
+    for (int i = 0; i < 100; i++)
+    {
+        sortedArray[i] = i;
+    }
+    smartQSort(sortedArray, 0, 99);
+
+    // Массив из одного элемента
+    int arrayOfOneElement[1] = { 109 };
+    smartQSort(arrayOfEqualValues, 0, 0);
+
+    return sortingСheck(arrayOfNumber, 10) == true
+        && sortingСheck(arrayOfEqualValues, 100) == true
+        && sortingСheck(sortedArray, 100) == true
+        && sortingСheck(arrayOfOneElement, 1) == true;
+}
+
 int main()
 {
+    if (!testCorrectQSort())
+    {
+        printf(" error");
+        return 0;
+    }
     int numberOfElements = 0;
     printf("Enter the number of elements in the array\n");
     scanf_s("%d", &numberOfElements);
     int* arrayOfNumber = (int*)calloc(numberOfElements, sizeof(int));
+    if (arrayOfNumber == NULL)
+    {
+        return -1;
+    }
     for (int i = 0; i < numberOfElements; i++)
     {
         arrayOfNumber[i] = rand();
