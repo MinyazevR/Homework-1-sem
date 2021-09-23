ecce2e7b5432793ed3d496e7fc8b2ad31fa1c97d
MinyazevR <eeuriset8@gmail.com>
2021-09-24 03:11:09 +0500

    quicksort with inserts

--- quickSort/quickSort/quicksort.c
+++ quickSort/quickSort/quicksort.c
@@ -1,35 +1,77 @@
 #include <stdio.h>
 #include <locale.h>
+#include <math.h>
 #include <stdbool.h>
 #include <malloc.h>
 #include <stdlib.h>
 
-int semiQSort(int* arrayOfNumber, int numberElementsInArray)
+void smartQSort(int* arrayOfNumber, int initialElement, int endElement)
 {
-    int increment = 1;
-    int decrement = numberElementsInArray - 1;
-
-    while (increment < decrement)
+    int increment = initialElement;
+    int decrement = endElement;
+    int supportElement = 0;
+    if (arrayOfNumber[increment] <= arrayOfNumber[increment + 1])
     {
-        /* второе условие цикла для того, что лишний раз не прибавлять increment, ведь массив уже может быть отсортирован и в
-        случае когда элементов больше опорного меньше чем элементов меньше опорного increment будет увеличиваться до countOfElementsLessFirst + 1 и только
-        после этого цикл остановится, т.е будет сделано много лишних инкрементов increment*/
-        while (arrayOfNumber[increment] < arrayOfNumber[0] && increment < decrement)
-        {
-            increment++;
-        }
-        while (arrayOfNumber[decrement] >= arrayOfNumber[0] && decrement > increment)
-        {
-            decrement--;
-        }
-        if (increment <= decrement)
-        {
-            const int temporaryVariable = arrayOfNumber[decrement];
-            arrayOfNumber[decrement] = arrayOfNumber[increment];
-            arrayOfNumber[increment] = temporaryVariable;
-        }
-        increment++;
-        decrement--;
+        supportElement = arrayOfNumber[increment + 1];
     }
-    return decrement;
+    else if (arrayOfNumber[increment] > arrayOfNumber[increment + 1])
+    {
+        supportElement = arrayOfNumber[increment];
+    }
+    if (endElement - initialElement + 1 >= 10)
+    {
+        while (increment < decrement)
+        {
+            while (arrayOfNumber[increment] < supportElement && increment <= decrement)
+            {
+                increment++;
+            }
+            while (arrayOfNumber[decrement] > supportElement && decrement >= increment)
+            {
+                decrement--;
+            }
+            if (increment <= decrement)
+            {
+                int temporaryVariable = arrayOfNumber[decrement];
+                arrayOfNumber[decrement] = arrayOfNumber[increment];
+                arrayOfNumber[increment] = temporaryVariable;
+                increment++;
+                decrement--;
+            }
+        }
+        if (initialElement < decrement)
+        {
+            smartQSort(arrayOfNumber, initialElement, decrement);
+        }
+        if (increment < endElement)
+        {
+            smartQSort(arrayOfNumber, increment, endElement);
+        }
+    }
+    else
+    {
+        for (int i = initialElement; i <= decrement; i++)
+        {
+            int elementToTheLeftOfKey = i - 1;
+            const int keyArray = arrayOfNumber[i];
+            while (arrayOfNumber[elementToTheLeftOfKey] > keyArray && elementToTheLeftOfKey >= 0)
+            {
+                arrayOfNumber[elementToTheLeftOfKey + 1] = arrayOfNumber[elementToTheLeftOfKey];
+                elementToTheLeftOfKey--;
+                arrayOfNumber[elementToTheLeftOfKey + 1] = keyArray;
+            }
+        }
+        for (int i = increment; i <= endElement; i++)
+        {
+            int elementToTheLeftOfKey = i - 1;
+            const int keyArray = arrayOfNumber[i];
+            while (arrayOfNumber[elementToTheLeftOfKey] > keyArray && elementToTheLeftOfKey >= 0)
+            {
+                arrayOfNumber[elementToTheLeftOfKey + 1] = arrayOfNumber[elementToTheLeftOfKey];
+                elementToTheLeftOfKey--;
+                arrayOfNumber[elementToTheLeftOfKey + 1] = keyArray;
+            }
+        }
+    }
 }
+
