e19166afe302628506f12c850d7e6990c87c714c
MinyazevR <eeuriset8@gmail.com>
2021-09-25 17:27:24 +0500

    Homework №2

---
+++ Homework №2/Fibonacci/Fibonacci/Fibonacci.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int sequenceFibonacciIteration(int numberOfSequence)
+{
+    // checking for the correctness of the entered number
+    if (numberOfSequence < 0)
+    {
+        printf("Input error.");
+        return -1;
+    }
+
+    int firstElementSequenceOfFibonacci = 1;
+    int secondElementSequenceOfFibonacci = 1;
+    int thirdElementSequenceOfFibonacci = 0;
+
+    for (int i = 0; i < numberOfSequence - 2; i++)
+    {
+        thirdElementSequenceOfFibonacci = firstElementSequenceOfFibonacci + secondElementSequenceOfFibonacci;
+        firstElementSequenceOfFibonacci = secondElementSequenceOfFibonacci;
+        secondElementSequenceOfFibonacci = thirdElementSequenceOfFibonacci;
+    }
+
+    if (numberOfSequence == 0)
+    {
+        return 0;
+    }
+
+    if (numberOfSequence > 0)
+    {
+        return secondElementSequenceOfFibonacci;
+    }
+}
+
+int sequenceFibonacciRecursion(int numberOfSequence)
+{
+    // checking for the correctness of the entered number
+    if (numberOfSequence < 0)
+    {
+        printf("Input error.");
+        exit(0);
+    }
+
+    if (numberOfSequence == 1)
+    {
+        return 1;
+    }
+
+    if (numberOfSequence == 0)
+    {
+        return 0;
+    }
+
+    return sequenceFibonacciRecursion(numberOfSequence - 1) + sequenceFibonacciRecursion(numberOfSequence - 2);
+}
+
+// function for checking the correctness of the program
+bool testCorrectFibonacci()
+{
+    return sequenceFibonacciIteration(1) == 1 && sequenceFibonacciRecursion(1) == 1
+    && sequenceFibonacciIteration(0) == 0 && sequenceFibonacciRecursion(0) == 0
+    && sequenceFibonacciIteration(12) == 144 && sequenceFibonacciRecursion(12) == 144
+    && sequenceFibonacciIteration(16) == 987
+    && sequenceFibonacciRecursion(20) == 6765;
+}
+
+int main()
+{
+    if (!testCorrectFibonacci())
+    {
+        printf("Verification failed");
+        return 0;
+    }
+    printf("Enter the number of the member of the Fibonacci sequence that you want to display on the screen\n");
+    int numberOfSequence = 0;
+    scanf_s("%d", &numberOfSequence);
+    printf("Iterative version : %d\n", sequenceFibonacciIteration(numberOfSequence));
+    printf("Recursive version : %d\n", sequenceFibonacciRecursion(numberOfSequence));
+}
