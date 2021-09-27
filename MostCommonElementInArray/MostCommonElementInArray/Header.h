b14892b3f6232407565c307aac2be7480b8e7a65
MinyazevR <eeuriset8@gmail.com>
2021-09-27 23:41:03 +0500

    fixed bugs

--- MostCommonElementInArray/MostCommonElementInArray/Header.h
+++ MostCommonElementInArray/MostCommonElementInArray/Header.h
@@ -1,9 +1,19 @@
+#pragma once
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-bool sortingCheck(int* arrayOfNumber, int numberOfElements);
-bool testCorrectQSort();
+// function for sorting an array
 void smartQSort(int* arrayOfNumber, int initialElement, int endElement);
+
+// function for checking the sorting of an array
+bool sortingCheck(int* arrayOfNumber, int numberOfElements);
+
+// checking the sorting operation
+bool testCorrectQSort();
+
+// a function for finding the most frequently occurring element in an array
 int searchMostCommonElementInArray(int* arrayOfNumber, int numberOfElements);
+
+// function to check the operation of the function searchMostCommonElementInArray
 bool testSearchMostCommonElement();
