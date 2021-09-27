2c2968ae7815e3f0675f0f41d74bb558c0ee4316
MinyazevR <eeuriset8@gmail.com>
2021-09-27 23:34:33 +0500

    fixed bugs

--- binarySearch/binarySearch/Header.h
+++ binarySearch/binarySearch/Header.h
@@ -1,12 +1,23 @@
+#pragma once
 #include <stdio.h>
 #include <stdbool.h>
 #include <malloc.h>
 #include <stdlib.h>
 #include <math.h>
 
+
+//function for sorting an array
 void smartQSort(int* arrayOfNumber, int initialElement, int endElement);
+
+//function for checking the sorting of an array
 bool sortingCheck(int* arrayOfNumber, int numberOfElements);
+
+// checking sorting operation
 bool testCorrectQSort();
+
+// check if binary search works
 bool testCorrectBinarySearch();
+
+// binary search function
 int binarySearch(int* arrayOfNumber, int numberOfElements, int requiredNumber);
 
