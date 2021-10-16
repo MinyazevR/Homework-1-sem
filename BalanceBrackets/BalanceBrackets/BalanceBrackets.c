a04a9a8cda8b9d27f28b30153cf6ee0ea2add991
MinyazevR <eeuriset8@gmail.com>
2021-10-17 00:25:34 +0500

    writing all functions, text and comments

---
+++ BalanceBrackets/BalanceBrackets/BalanceBrackets.c
@@ -0,0 +1,60 @@
+#include "BalanceBrackets.h"
+#include "Stack.h"
+#include "StackTest.h"
+#include "BalanceBracketsTest.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+bool checkCorrectOrderBrackets(char* expressionFromParentheses)
+{
+    Stack* head = NULL;
+    int counter = 0;
+    while (expressionFromParentheses[counter] != '\0')
+    {
+        if (expressionFromParentheses[counter] == '(' || expressionFromParentheses[counter] == '{'
+            || expressionFromParentheses[counter] == '[')
+        {
+            push(&head, expressionFromParentheses[counter]);
+        }
+        else if (expressionFromParentheses[counter] == ')' || expressionFromParentheses[counter] == '}'
+                 || expressionFromParentheses[counter] == ']')
+        {
+            if(isEmpty(head))
+            {
+                return false;
+            }
+            char k = pop(&head);
+            if ((k == '(' && expressionFromParentheses[counter] != ')')
+                || (k == '{' && expressionFromParentheses[counter] != '}')
+                || (k == '[' && expressionFromParentheses[counter] != ']'))
+            {
+                return false;
+            }
+        }
+        counter++;
+    }
+    if (isEmpty(head))
+    {
+        deleteStack(&head);
+        return true;
+    }
+    deleteStack(&head);
+    return false;
+}
+int main()
+{
+    if (!balanceBracketsTest() || !popTest() || !pushTest() || !deleteStackTest())
+    {
+        printf("Test failed");
+        return -1;
+    }
+    char expressionFromParentheses[250] = { '\0' };
+    printf("enter the expression in postfix form\n");
+    scanf_s("%s", expressionFromParentheses, (unsigned)_countof(expressionFromParentheses));
+    if (!checkCorrectOrderBrackets(expressionFromParentheses))
+    {
+        printf("incorrect");
+        return 0;
+    }
+    printf("correct");
+}
