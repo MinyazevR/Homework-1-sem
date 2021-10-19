2d086b69dcbe8dffa70e10f9119d17e1947d422e
MinyazevR <eeuriset8@gmail.com>
2021-10-19 17:37:15 +0500

    fixed

--- mostElemenInArray/mostElemenInArray/qsort.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-// function for sorting an array
-void qSort(int* arrayOfNumber, int numberOfElements);
--- mostElemenInArray/mostElemenInArray/testTheMostCommonElement.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <stdbool.h>
-
-// test for function searchMostCommonElementInArray
-bool testSearchMostCommonElement();
