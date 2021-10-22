400b92a6edf9d55085c64bfabf5637139f9ddec2
MinyazevR <eeuriset8@gmail.com>
2021-10-23 00:51:48 +0500

    start

---
+++ sortStation/sortStation/SortStation.c
@@ -0,0 +1,31 @@
+#include "../../Stack/Stack/Stack.h"
+#include "../../Stack/Stack/StackTest.h"
+#include <stdio.h>
+#include <malloc.h>
+
+char* sortStation(char* array)
+{
+    Stack* head = NULL;
+    int counterForTheOutputArray = 0;
+    int counter = 0;
+    char* arrayToOutput = (char*)malloc(1000*sizeof(char));
+    while (array[counter] != '\0')
+    {
+        if (array[counter] >= '0' || array[counter] <= '9')
+        {
+            arrayToOutput[counterForTheOutputArray] = array[counter];
+            counterForTheOutputArray;
+        }
+        if(array[counter] == '-' || array[counter] == '+')
+        
+    }
+}
+
+int main()
+{
+    char array[1000] = {'\0'};
+    printf("aa\n");
+    scanf("%s", array);
+    sortStation(array);
+
+}
