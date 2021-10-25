98a42e2a2370424c3303c487655c239a5d9a5f3a
MinyazevR <eeuriset8@gmail.com>
2021-10-25 18:45:56 +0500

    adding const

--- Stack/Stack/StackTest.c
+++ Stack/Stack/StackTest.c
@@ -6,9 +6,9 @@ bool pushTest()
 {
     Stack* head = NULL;
     push(&head, 12);
-    float firstResult = head->value;
+    const float firstResult = head->value;
     push(&head, 128);
-    float secondResult = head->value;
+    const float secondResult = head->value;
     deleteStack(&head);
     return firstResult == 12 && secondResult == 128;
 }
@@ -20,18 +20,18 @@ bool popTest()
     push(&head, 128);
     push(&head, 147);
     int error = 0;
-    float firstPopResult = pop(&head, &error);
+    const float firstPopResult = pop(&head, &error);
     if (error == 1)
     {
         return false;
     }
-    float firstUpperElement = head->value;
-    float secondPopResult = pop(&head, &error);
+    const float firstUpperElement = head->value;
+    const float secondPopResult = pop(&head, &error);
     if (error == 1)
     {
         return false;
     }
-    float secondUpperElement = head->value;
+    const float secondUpperElement = head->value;
     deleteStack(&head);
     return firstPopResult == 147 && firstUpperElement == 128 && secondPopResult == 128 && secondUpperElement == 12;
 }
