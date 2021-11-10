37c287fbc7a7344cb3bf765991ea562b5ad7493a
MinyazevR <eeuriset8@gmail.com>
2021-11-10 17:44:28 +0500

    the basis is written

---
+++ MergeSort/MergeSort/Main.c
@@ -0,0 +1,77 @@
+#include "MergeSort.h"
+#include "TestMergeSort.h"
+#include <stdio.h>
+#include <locale.h>
+
+void readPhonebook(List* list, const char* fileName, int* error)
+{
+    FILE* file = fopen(fileName, "r");
+    if (file == NULL)
+    {
+        *error = 1;
+        return;
+    }
+    while (!feof(file))
+    {
+        char arrayForName[100] = { '\0' };
+        char arrayForNumber[100] = { '\0' };
+        if (fscanf(file, "%s", arrayForName) != EOF);
+        {
+            const int fscanfResult = fscanf(file, "%s", arrayForNumber);
+            if (fscanfResult == 0)
+            {
+                break;
+            }
+        }
+        add(list, arrayForName, arrayForNumber, error);
+        if (*error == 3)
+        {
+            fclose(file);
+            return;
+        }
+    }
+    fclose(file);
+}
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    if (!testMergeSort())
+    {
+        printf("Тест провален");
+        return -1;
+    }
+    List* newList = createList();
+    int error = 0;
+    readPhonebook(newList, "Phonebook.txt", &error);
+    printf("Вы хотите отсортировать по имени или номеру?\n");
+    printf("Нажмите 1 если по имени, другую цифру если по номеру\n");
+    int number = 0;
+    const int scanResult = scanf("%d", &number);
+    if (scanResult == 0)
+    {
+        printf("Не получилось прочитать число");
+        return -1;
+    }
+    List* answer = mergeSort(newList, number, &error);
+    if (error == 1)
+    {
+        deleteList(answer);
+        printf("Файл не найден");
+        return -1;
+    }
+    if (error == 2)
+    {
+        deleteList(answer);
+        printf("Ошибка в работе программы");
+        return -1;
+    }
+    if (error == 3)
+    {
+        deleteList(answer);
+        printf("Недостаточно памяти");
+        return -1;
+    }
+    print(answer);
+    printf("\n");
+    deleteList(answer);
+}
---
+++ MergeSort/MergeSort/TestMergeSort.c
@@ -0,0 +1,67 @@
+#include "TestMergeSort.h"
+#include "MergeSort.h"
+#include "../../NewList/NewList/List.h"
+
+bool testMergeSort()
+{
+    List* newFirstList = createList();
+    int error = 0;
+    add(newFirstList, "Pavel", "890345", &error);
+    if (error == 3)
+    {
+        deleteList(newFirstList);
+        return false;
+    }
+    add(newFirstList, "Alex", "765494", &error);
+    if (error == 3)
+    {
+        deleteList(newFirstList);
+        return false;
+    }
+    add(newFirstList, "Artem", "956469", &error);
+    if (error == 3)
+    {
+        deleteList(newFirstList);
+        return false;
+    }
+    add(newFirstList, "Ruslan", "2342424", &error);
+    if (error == 3)
+    {
+        deleteList(newFirstList);
+        return false;
+    }
+    List* newSecondList = createList();
+    add(newSecondList, "Alex", "765494", &error);
+    if (error == 3)
+    {
+        deleteList(newFirstList);
+        deleteList(newSecondList);
+        return false;
+    }
+    add(newSecondList, "Artem", "956469", &error);
+    if (error == 3)
+    {
+        deleteList(newFirstList);
+        deleteList(newSecondList);
+        return false;
+    }
+    add(newSecondList, "Pavel", "890345", &error);
+    if (error == 3)
+    {
+        deleteList(newFirstList);
+        deleteList(newSecondList);
+        return false;
+    }
+    add(newSecondList, "Ruslan", "2342424", &error);
+    if (error == 3)
+    {
+        deleteList(newFirstList);
+        deleteList(newSecondList);
+        return false;
+    }
+    List* firstList = mergeSort(newFirstList, 1, &error);
+    bool result = compareList(firstList, newSecondList);
+    deleteList(newSecondList);
+    deleteList(firstList);
+    return result;
+}
