fa4fefe2b5b7b7449fb66e89908ab09399fca304
MinyazevR <eeuriset8@gmail.com>
2021-11-27 16:11:40 +0300

    a list has been written to implement a hash table

---
+++ SinglyLinkedListForHashTable/SinglyLinkedListForHashTable/SinglyLinkedListForHashTable.c
@@ -0,0 +1,237 @@
+#include "SinglyLinkedListForHashTable.h"
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+// Structure containing pointers to the beginning and end of the list
+typedef struct List
+{
+    int size;
+    struct ListElement* head;
+    struct ListElement* tail;
+} List;
+
+// Structure containing a pointer to the next list item and a value variable for the list items
+typedef struct ListElement
+{
+    int numberOfDuplicateValues;
+    char* value;
+    struct ListElement* next;
+} ListElement;
+
+// Structure containing a pointer to a ListElement
+typedef struct Position
+{
+    ListElement* position;
+} Position;
+
+List* createList()
+{
+    return calloc(1, sizeof(List));
+}
+
+void deleteList(List* list)
+{
+    ListElement* position = list->head;
+    while (position != NULL)
+    {
+        list->head = list->head->next;
+        free(position->value);
+        free(position);
+        position = list->head;
+    }
+    free(list);
+}
+
+void deletePosition(Position* position)
+{
+    free(position);
+}
+
+void removeFirstElement(List* list, Error* error)
+{
+    if (*error != NOT_ERROR)
+    {
+        return;
+    }
+    if (list->head == NULL)
+    {
+        *error = EMPTY_LIST;
+        return;
+    }
+    if (list->head == list->tail)
+    {
+        list->size = 0;
+        free(list->head->value);
+        free(list->head);
+        list->head = NULL;
+        list->tail = NULL;
+        return;
+    }
+    ListElement* element = list->head;
+    list->head = list->head->next;
+    list->size--;
+    free(element->value);
+    free(element);
+
+}
+
+Position* first(List* list, Error* error)
+{
+    if (*error != NOT_ERROR)
+    {
+        return NULL;
+    }
+    Position* positionFirst = malloc(sizeof(Position));
+    if (positionFirst == NULL)
+    {
+        *error = EMPTY_LIST;
+        return NULL;
+    }
+    positionFirst->position = list->head;
+    return positionFirst;
+}
+
+Position* last(List* list, Error* error)
+{
+    if (*error != NOT_ERROR)
+    {
+        return NULL;
+    }
+    Position* positionLast = malloc(sizeof(Position));
+    if (positionLast == NULL)
+    {
+        *error = EMPTY_LIST;
+        return NULL;
+    }
+    positionLast->position = list->tail;
+    return positionLast;
+}
+
+Position* next(Position* position)
+{
+    position->position = position->position->next;
+    return position;
+}
+
+bool isLastElement(Position* position)
+{
+    return position->position->next == NULL;
+}
+
+int numberOfElements(List* list)
+{
+    return list->size;
+}
+
+char* getHeadValue(List* list)
+{
+    if (list->head == NULL)
+    {
+        return NULL;
+    }
+    return list->head->value;
+}
+
+void add(List* list, const char* value, Error* error)
+{
+    if (*error != NOT_ERROR)
+    {
+        return;
+    }
+    if (list->head == NULL)
+    {
+        char* valueCopy = calloc(strlen(value) + 1, sizeof(char));
+        if (valueCopy == NULL)
+        {
+            *error = INSUFFICIENT_MEMORY;
+            return;
+        }
+        strcpy(valueCopy, value);
+        ListElement* newElement = calloc(1, sizeof(ListElement));
+        if (newElement == NULL)
+        {
+            free(valueCopy);
+            *error = INSUFFICIENT_MEMORY;
+            return;
+        }
+        newElement->value = valueCopy;
+        list->size = 1;
+        list->head = newElement;
+        list->tail = newElement;
+        list->head->numberOfDuplicateValues = 1;
+        return;
+    }
+    ListElement* head = list->head;
+    int counter = 0;
+    for (counter = 1; counter < list->size; counter++)
+    {
+        if (strcmp(value, head->value) == 0)
+        {
+            head->numberOfDuplicateValues++;
+            return;
+        }
+        head = head->next;
+    }
+    if (counter == list->size)
+    {
+        char* valueCopy = calloc(strlen(value) + 1, sizeof(char));
+        if (valueCopy == NULL)
+        {
+            *error = INSUFFICIENT_MEMORY;
+            return;
+        }
+        strcpy(valueCopy, value);
+        ListElement* newElement = calloc(1, sizeof(ListElement));
+        if (newElement == NULL)
+        {
+            free(valueCopy);
+            *error = INSUFFICIENT_MEMORY;
+            return;
+        }
+        newElement->value = valueCopy;
+        list->size++;
+        list->tail->next = newElement;
+        list->tail = list->tail->next;
+        newElement->numberOfDuplicateValues = 1;
+    }
+}
+
+char* getValue(Position* position)
+{
+    return position->position->value;
+}
+
+bool isEmpty(List* list)
+{
+    return list->head == NULL;
+}
+
+bool isOneElement(List* list)
+{
+    return list->head->next == NULL;
+}
+
+void print(List* list)
+{
+    ListElement* element = list->head;
+    while (element != NULL)
+    {
+        printf("%s - %d\n", element->value, element->numberOfDuplicateValues);
+        element = element->next;
+    }
+}
+
+const char* decodingError(Error error)
+{
+    if (error == EMPTY_LIST)
+    {
+        return "Error in the program";
+    }
+    if (error == INSUFFICIENT_MEMORY)
+    {
+        return "Memory not allocated";
+    }
+    return NULL;
+}
---
+++ SinglyLinkedListForHashTable/SinglyLinkedListForHashTable/TestSinglyLinkedListForHashTable.c
@@ -0,0 +1,100 @@
+#include "TestSinglyLinkedListForHashTable.h"
+#include "SinglyLinkedListForHashTable.h"
+#include <string.h>
+
+// Function to check the function that adds an item to the list
+bool testAdd()
+{
+    Error error = NOT_ERROR;
+    List* newList = createList();
+    add(newList, "test", &error);
+    add(newList, "list", &error);
+    if (error == INSUFFICIENT_MEMORY)
+    {
+        deleteList(newList);
+        return false;
+    }
+    const char* firstValue = getHeadValue(newList);
+    Position* position = first(newList, &error);
+    if (error == EMPTY_LIST)
+    {
+        deleteList(newList);
+        return false;
+    }
+    const char* secondValue = getValue(next(position));
+    bool result = strcmp(firstValue, "test") == 0 && strcmp(secondValue, "list") == 0;
+    deletePosition(position);
+    deleteList(newList);
+    return result;
+}
+
+// Function to check the function that deletes the first item in the list
+bool testRemoveHead()
+{
+    Error error = NOT_ERROR;
+    List* newList = createList();
+    add(newList, "Hello", &error);
+    add(newList, "World", &error);
+    add(newList, "Hello", &error);
+    print(newList);
+    removeFirstElement(newList, &error);
+    if (error == EMPTY_LIST || error == INSUFFICIENT_MEMORY)
+    {
+        deleteList(newList);
+        return false;
+    }
+    const char* value = getHeadValue(newList);
+    bool result = strcmp(value, "World") == 0;
+    deleteList(newList);
+    return result;
+}
+
+// Function for checking the function counting the number of elements
+bool testNumberOfElements()
+{
+    List* newList = createList();
+    Error error = NOT_ERROR;
+    add(newList, "Hello", &error);
+    add(newList, "Merge", &error);
+    add(newList, "test",  &error);
+    add(newList, "list",  &error);
+    if (error == INSUFFICIENT_MEMORY)
+    {
+        deleteList(newList);
+        return false;
+    }
+    const int number = numberOfElements(newList);
+    deleteList(newList);
+    return number == 4;
+}
+
+// Function that checks the function that returns the value of the first element
+bool testGetValue()
+{
+    List* newList = createList();
+    Error error = NOT_ERROR;
+    add(newList, "Hello", &error);
+    add(newList, "World", &error);
+    if (error == INSUFFICIENT_MEMORY)
+    {
+        deleteList(newList);
+        return false;
+    }
+    Position* position = first(newList, &error);
+    if (error == EMPTY_LIST)
+    {
+        deleteList(newList);
+        return false;
+    }
+    const char* firstValue = getHeadValue(newList);
+    const char* secondValue = getValue(next(position));
+    bool result = strcmp(firstValue, "Hello") == 0 && strcmp(secondValue, "World") == 0;
+    deleteList(newList);
+    deletePosition(position);
+    return result;
+}
+
+bool allTest()
+{
+    return testAdd() && testGetValue() && testNumberOfElements() && testRemoveHead();
+}
