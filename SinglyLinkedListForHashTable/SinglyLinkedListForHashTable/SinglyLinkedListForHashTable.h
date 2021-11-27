95cf48bd8e7c295354b99529872901e403c65693
MinyazevR <eeuriset8@gmail.com>
2021-11-27 20:55:53 +0300

    Revert "added a function that determines whether an item is in the list"
    This reverts commit 0a7f1609b2303c3171b479877ff97354b6be1c29.

--- SinglyLinkedListForHashTable/SinglyLinkedListForHashTable/SinglyLinkedListForHashTable.h
+++ SinglyLinkedListForHashTable/SinglyLinkedListForHashTable/SinglyLinkedListForHashTable.h
@@ -57,6 +57,3 @@ char* getValue(Position* position);
 
 // Function for error decoding
 const char* decodingError(Error error);
-
-// Function to check whether an item is in the list
-bool inList(List* list, const char* value);
