234663687006e9cc4feac29f806e08cbfe20e192
MinyazevR <eeuriset8@gmail.com>
2021-12-17 11:29:50 +0300

    added comments to functions

--- Homework№12/lexicalAnalysis/lexicalAnalysis/LexicalAnalysis.h
+++ Homework№12/lexicalAnalysis/lexicalAnalysis/LexicalAnalysis.h
@@ -1,4 +1,5 @@
 #pragma once
 #include <stdbool.h>
 
+// Function to check whether an expression is a real number
 bool checkExpression(char* string);
--- Homework№12/lexicalAnalysis/lexicalAnalysis/ReadString.h
+++ Homework№12/lexicalAnalysis/lexicalAnalysis/ReadString.h
@@ -1,3 +1,4 @@
 #pragma once
 
+// Function for entering a line with memory allocation (for entering a line of arbitrary size)
 char* readString(int* error);
